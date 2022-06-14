a6b130ef4e630290a83f73970a672a80172d23b0
Nikita Ryzhenko <nick.ryzhenko1992@gmail.com>
2022-06-15 00:01:59 +0300

    done

--- src/main/java/app/model/Timer.java
+++ src/main/java/app/model/Timer.java
@@ -1,8 +1,16 @@
 package app.model;
 
-public class Timer {
 
-    private Long nanoTime = System.nanoTime();
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+@Component
+public class Timer {
+    private Timer timer;
+    public Timer getTimer(){
+        return timer;
+    }
+
+    private final Long nanoTime = System.nanoTime();
 
     public Long getTime() {
         return nanoTime;
